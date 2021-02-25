c13609fb2f79f5eed626af6c84953a4dc0ac04ab
Esteban <ebedoya83134@umanizales.edu.co>
2021-02-24 21:14:17 -0500

    Changed the code to make use of arrays.

--- sales-system/Program.cs
+++ sales-system/Program.cs
@@ -7,31 +7,31 @@ namespace sales_system
         static void Main(string[] args)
         {
             //creating sellers
-            Seller MySeller_1 = new Seller("Fulano", 21);
-            Seller MySeller_2 = new Seller("Sultano", 18);
-            Seller MySeller_3 = new Seller("Mengano", 26);
+            Seller[] Sellers = new Seller[3];
+            Sellers[0] = new Seller("Fulano", 21);
+            Sellers[1] = new Seller("Sultano", 18);
+            Sellers[2] = new Seller("Mengano", 26);
 
             //creating products
-            Product MyProduct_1 = new Product("Soy Sauce (750 ml)", 11.99);
-            Product MyProduct_2 = new Product("Instant Ramen (500 grams)", 5.50);
-            Product MyProduct_3 = new Product("Instant Ramen (2000 grams)", 19.99);
+            Product[] Products = new Product[3];
+            Products[0] = new Product("Soy Sauce (750 ml)", 11.99);
+            Products[1] = new Product("Instant Ramen (500 grams)", 5.50);
+            Products[2] = new Product("Instant Ramen (2000 grams)", 19.99);
 
             //creating sales
-            Sale MySale_1 = new Sale(MyProduct_1, MySeller_1, "-");
-            Sale MySale_2 = new Sale(MyProduct_2, MySeller_2, "-");
-            Sale MySale_3 = new Sale(MyProduct_3, MySeller_3, "-");
+            Sale[] Sales = new Sale[3];
+            for (int i = 0; i < Sales.Length; i++)
+            {
+                Sales[i] = new Sale(Products[i], Sellers[i], "-");
+            }
 
             //calculating total and printing to the console
             double total = 0;
-
-            total += MySale_1.GetProduct().GetPrice();
-            PrintProductPrice(MySale_1);
-
-            total += MySale_2.GetProduct().GetPrice();
-            PrintProductPrice(MySale_2);
-
-            total += MySale_3.GetProduct().GetPrice();
-            PrintProductPrice(MySale_3);
+            for (int i = 0; i < Sales.Length; i++)
+            {
+                total += Sales[i].GetProduct().GetPrice();
+                PrintProductPrice(Sales[i]);
+            }
 
             Console.WriteLine("Total: " + Math.Round(total,2));
 
